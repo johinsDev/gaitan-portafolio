a16ca399abd1bf0ec04c2482e4c5ab2a1b508471
Johan Villamil <johinsdev@gmail.com>
2024-09-03 22:39:24 -0500

    refactor: Improve image loading and styling in Home page

--- src/app/(website)/(home)/page.tsx
+++ src/app/(website)/(home)/page.tsx
@@ -14,7 +14,7 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col pb-16">
       <Suspense fallback={<HeroSkeleton />}>
         <Hero />
       </Suspense>
