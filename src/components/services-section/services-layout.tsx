0bfe83f7e6b2a4b6509a4d59a103df5ec9a1ffce
Johan Villamil <johinsdev@gmail.com>
2024-09-03 21:11:44 -0500

    feat: Add lazy loading for images in services section carousel

--- src/components/services-section/services-layout.tsx
+++ src/components/services-section/services-layout.tsx
@@ -2,6 +2,7 @@
 import { urlForImage } from "@/sanity/lib/utils";
 import { ServicesSection } from "@/types";
 import Image from "next/image";
+import { Carousel, CarouselContent, CarouselItem } from "../carousel";
 import { CustomPortableText } from "../shared/CustomPortableText";
 
 type Props = {
@@ -22,35 +23,43 @@ export function ServicesSectionLayout({ data }: Props) {
         </div>
       )}
       <div className="main_container mt-12">
-        <div className="grid lg:grid-cols-3 gap-8">
-          {data.services?.map((service) => {
-            const image = service?.image?.image?.asset &&
-              urlForImage(service?.image?.image)?.width(600).url();
+        <Carousel className="w-full">
+          <CarouselContent className="-ml-8">
+            {data.services?.map((service, index) => {
+              const image = service?.image?.image?.asset &&
+                urlForImage(service?.image?.image)?.width(600).url();
 
-            if (!image) return null;
+              if (!image) return null;
 
-            return (
-              <div key={service._key} className="text-center">
-                <div className="aspect-video relative rounded-3xl overflow-hidden">
-                  <Image
-                    src={image}
-                    fill
-                    alt={service.image?.alt ?? ""}
-                    className="object-cover"
-                  />
-                </div>
-                <div className="text-2xl text-left lg:text-center md:text-3xl font-bold mt-4">
-                  {service.title}
-                </div>
-                {!!service.description && (
-                  <div className="text-left lg:text-center text-lg mt-4">
-                    <CustomPortableText value={service.description as any} />
+              return (
+                <CarouselItem
+                  key={index}
+                  className="basis-3/4 lg:basis-1/3 pl-8"
+                >
+                  <div key={service._key} className="text-center">
+                    <div className="aspect-video relative rounded-3xl overflow-hidden">
+                      <Image
+                        src={image}
+                        fill
+                        alt={service.image?.alt ?? ""}
+                        className="object-cover"
+                      />
+                    </div>
+                    <div className="text-2xl text-left lg:text-center md:text-3xl font-bold mt-4">
+                      {service.title}
+                    </div>
+                    {!!service.description && (
+                      <div className="text-left lg:text-center text-lg mt-4">
+                        <CustomPortableText value={service.description as any} />
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-            )
-          })}
-        </div>
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+        </Carousel>
+
       </div>
     </section >
   );
